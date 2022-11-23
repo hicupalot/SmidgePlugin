3aa806d2535968a9bba5f5d0696f9993180c01c4
hicupalot <hicupalot@gmail.com>
2022-11-23 08:44:29 +0000

    Fixed /spy and cleaned up plugin.yml

--- src/main/java/Follower.java
+++ src/main/java/Follower.java
@@ -5,41 +5,57 @@ import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-public class Follower implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Follower implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.DARK_RED + "You Must Be a Player to Do This!");
+            sender.sendMessage(Config.notPlayer);
             return false;
         }
-        if (!sender.hasPermission("smidge.spy")) {
-            sender.sendMessage(ChatColor.DARK_RED + "[Smidge] You do not have permission");
+        if (!sender.hasPermission("smidge.staff")) {
+            sender.sendMessage(Config.noPermission);
             return false;
         }
-        String username = args[0];
-        Player target = Bukkit.getPlayer(args[0]);
-
-        if (args.length <0) {
+        if (args.length == 0) {
             sender.sendMessage(ChatColor.RED + "[Smidge] Command Usage /spy (player)");
-            return false;
+        } else {
+            Player target = Bukkit.getPlayerExact(args[0]);
+            if (target == null) {
+                sender.sendMessage(ChatColor.RED + "[Smidge] This is not an online player");
+                return false;
+            }
+            if (((Player) sender).getUniqueId().equals(target.getUniqueId())) {
+                sender.sendMessage(ChatColor.RED + "[Smidge] You cannot spy on yourself!");
+                return false;
+            } else {
+                VanishAPI.hidePlayer((Player) sender);
+                ((Player) sender).setGameMode(GameMode.SPECTATOR);
+                ((Player) sender).teleport(target);
+                sender.sendMessage(ChatColor.GOLD + "[Smidge] You are now monitoring " + target.getDisplayName());
+                return true;
+            }
         }
-        if (args.length > 1) {
-            sender.sendMessage(ChatColor.RED + "[Smidge] Command Usage /spy (player)");
-        }
-        if (!target.isOnline() || target == null) {
-            sender.sendMessage(ChatColor.RED + "[Smidge] This is not an online player!");
-            return false;
-        }
-        if (((Player) sender).getUniqueId().equals(target.getUniqueId())) {
-            sender.sendMessage(ChatColor.RED + "[Smidge] You cannot spy on yourself!");
-            return false;
-        }
-            VanishAPI.hidePlayer((Player) sender);
-            ((Player) sender).setGameMode(GameMode.SPECTATOR);
-            ((Player) sender).setSpectatorTarget(target);
-            sender.sendMessage(ChatColor.GOLD + "[Smidge] You are now monitoring " + username);
-            return true;
-        }
+        return true;
     }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 0) {
+            List<String> playerNames = new ArrayList<>();
+            Player[] players = new Player[Bukkit.getServer().getOnlinePlayers().size()];
+            Bukkit.getServer().getOnlinePlayers().toArray(players);
+            for (int i = 0; i < players.length; i++) {
+                playerNames.add(players[i].getName());
+            }
+
+            return playerNames;
+        }
+ return null;
+    }
+}
